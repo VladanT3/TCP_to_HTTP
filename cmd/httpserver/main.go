03edc4356087d887c64b5699b7037e8d5d4de1c5
VladanT3 <vladt.t33@gmail.com>
2025-03-26 20:34:21 +0100

    supposed to parse actual requests and send some predetermined responses but IT DOESNT DONT KNOW WHY

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/VladanT3/TCP_to_HTTP/internal/request"
 	"github.com/VladanT3/TCP_to_HTTP/internal/server"
 )
+
+func handler(w io.Writer, req *request.Request) *server.HandlerError {
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		return &server.HandlerError{StatusCode: 400, Message: "Your problem is not my problem\n"}
+	} else if req.RequestLine.RequestTarget == "/myproblem" {
+		return &server.HandlerError{StatusCode: 500, Message: "Woopsie, my bad\n"}
+	} else {
+		_, err := w.Write([]byte("All good, frfr\n"))
+		if err != nil {
+			return &server.HandlerError{StatusCode: 500, Message: err.Error()}
+		}
+	}
+	return nil
+}
 
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, handler)
 	if err != nil {
 		log.Fatal("Error starting server: ", err)
 	}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,20 +1,31 @@
 package server
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net"
 	"strconv"
 	"sync/atomic"
 
+	"github.com/VladanT3/TCP_to_HTTP/internal/request"
 	"github.com/VladanT3/TCP_to_HTTP/internal/response"
 )
 
 type Server struct {
 	Listener net.Listener
+	Handler  Handler
 	Running  atomic.Bool
 }
 
-func Serve(port int) (*Server, error) {
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 	if err != nil {
 		return nil, err
@@ -22,6 +33,7 @@ func Serve(port int) (*Server, error) {
 
 	s := &Server{
 		Listener: listener,
+		Handler:  handler,
 	}
 	s.Running.Store(true)
 
@@ -52,15 +64,63 @@ func (s *Server) listen() {
 }
 
 func (s *Server) handle(conn net.Conn) {
-	err := response.WriteStatusLine(conn, 200)
+	defer conn.Close()
+
+	r, err := request.RequestFromReader(conn)
 	if err != nil {
-		log.Println(err)
+		h_err := &HandlerError{
+			StatusCode: response.BadRequest,
+			Message:    err.Error(),
+		}
+		h_err.writeError(conn)
+		return
 	}
 
-	err = response.WriteHeaders(conn, response.GetDefaultHeaders(0))
-	if err != nil {
-		log.Println(err)
+	buf := bytes.NewBuffer([]byte{})
+	handler_err := s.Handler(buf, r)
+	if handler_err != nil {
+		handler_err.writeError(conn)
+		return
 	}
 
-	conn.Close()
+	b := buf.Bytes()
+	headers := response.GetDefaultHeaders(len(b))
+
+	err = response.WriteStatusLine(conn, 200)
+	if err != nil {
+		log.Println("Error writing response status line:", err)
+		return
+	}
+
+	err = response.WriteHeaders(conn, headers)
+	if err != nil {
+		log.Println("Error writing response headers:", err)
+		return
+	}
+
+	_, err = conn.Write(b)
+	if err != nil {
+		log.Println("Error writing response body:", err)
+	}
+}
+
+func (h_err HandlerError) writeError(w io.Writer) {
+	err := response.WriteStatusLine(w, h_err.StatusCode)
+	if err != nil {
+		log.Println("Error writing response status line:", err)
+		return
+	}
+
+	headers := response.GetDefaultHeaders(len(h_err.Message))
+	err = response.WriteHeaders(w, headers)
+	if err != nil {
+		log.Println("Error writing response headers:", err)
+		return
+	}
+
+	_, err = w.Write([]byte(h_err.Message))
+	if err != nil {
+		log.Println("Error writing response body:", err)
+		return
+	}
 }
