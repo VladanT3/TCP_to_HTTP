751712d1154c871b336c22f2f191e76daeb671ce
VladanT3 <vladt.t33@gmail.com>
2025-03-27 16:54:19 +0100

    pinpointed the problem to be with the conn writer will see tomorrow or something what it is

--- internal/server/server.go
+++ internal/server/server.go
@@ -64,14 +64,18 @@ func (s *Server) listen() {
 }
 
 func (s *Server) handle(conn net.Conn) {
+	log.Println("ENTERED SERVER.HANDLE FUNC\n============================================\n")
 	defer conn.Close()
 
 	r, err := request.RequestFromReader(conn)
+	log.Printf("Request: %v\n", r)
 	if err != nil {
+		log.Println("Error parsing request.")
 		h_err := &HandlerError{
 			StatusCode: response.BadRequest,
 			Message:    err.Error(),
 		}
+		log.Printf("Handler error: %v\n", h_err)
 		h_err.writeError(conn)
 		return
 	}
@@ -79,37 +83,51 @@ func (s *Server) handle(conn net.Conn) {
 	buf := bytes.NewBuffer([]byte{})
 	handler_err := s.Handler(buf, r)
 	if handler_err != nil {
+		log.Println("Handler error isnt nil.")
+		log.Printf("Handler error: %v\n", handler_err)
 		handler_err.writeError(conn)
 		return
 	}
 
 	b := buf.Bytes()
+	log.Printf("What we got back from the handler: %s\n", b)
+	log.Printf("Len of b: %d\n", len(b))
 	headers := response.GetDefaultHeaders(len(b))
+	log.Printf("Headers: %v\n", headers)
 
 	err = response.WriteStatusLine(conn, 200)
 	if err != nil {
 		log.Println("Error writing response status line:", err)
 		return
 	}
+	log.Println("Wrote status line.")
 
 	err = response.WriteHeaders(conn, headers)
 	if err != nil {
 		log.Println("Error writing response headers:", err)
 		return
 	}
+	log.Println("Wrote headers.")
 
-	_, err = conn.Write(b)
+	test := bytes.NewBuffer([]byte{})
+	multi_writer := io.MultiWriter(conn, test)
+	_, err = multi_writer.Write(b)
 	if err != nil {
 		log.Println("Error writing response body:", err)
 	}
+	log.Println("Wrote body.")
+	log.Printf("Wrote: %s\n", test.Bytes())
+	log.Println("EXITING SERVER.HANDLE FUNC\n=======================================\n")
 }
 
 func (h_err HandlerError) writeError(w io.Writer) {
+	log.Println("\tENTERED WRITE ERROR FUNC\n\t========================================\n")
 	err := response.WriteStatusLine(w, h_err.StatusCode)
 	if err != nil {
 		log.Println("Error writing response status line:", err)
 		return
 	}
+	log.Println("\tWrote status line.")
 
 	headers := response.GetDefaultHeaders(len(h_err.Message))
 	err = response.WriteHeaders(w, headers)
@@ -117,10 +135,16 @@ func (h_err HandlerError) writeError(w io.Writer) {
 		log.Println("Error writing response headers:", err)
 		return
 	}
+	log.Println("\tWrote headers.")
 
-	_, err = w.Write([]byte(h_err.Message))
+	test := bytes.NewBuffer([]byte{})
+	multi_writer := io.MultiWriter(w, test)
+	_, err = multi_writer.Write([]byte(h_err.Message))
 	if err != nil {
 		log.Println("Error writing response body:", err)
 		return
 	}
+	log.Println("\tWrote body.")
+	log.Printf("\tWrote: %s\n", test.Bytes())
+	log.Println("\tEXITING WRITE ERROR FUNC\n\t============================================\n")
 }
